ef8246cd2cd49b9230bb268ff6cb9be3897ec3c0
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-03 23:54:16 +0000

    fix bug task 0

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python3
-''' 
+""" 
 Description: function add that takes a float a and a float b
 Arguments: a: float, b: float
-'''
+"""
 
 
 def add(a: float, b: float) -> float:
-    ''' Return sum of a and b. '''
-    
+    """Return sum of a and b. """
     return a + b
