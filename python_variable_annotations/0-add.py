8cfc30dc8643f3c50de54335b5e19b6f416487d0
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-03 23:47:41 +0000

    task 0 - add

---
+++ python_variable_annotations/0-add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+''' 
+Description: function add that takes a float a and a float b
+Arguments: a: float, b: float
+'''
+
+
+def add(a: float, b: float) -> float:
+    ''' Return sum of a and b. '''
+    
+    return a + b
