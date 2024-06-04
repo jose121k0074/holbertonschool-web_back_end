2d2776484a079ee976344b3ff42741e8baec9d68
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-04 20:47:45 +0000

    readme and task 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -9,7 +9,8 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Generator that yields a random value between 0 and 10 every second"""
+    """Generator that yields a random value betwen
+       0 and 10 every second"""
     for i in range(10):
         await sleep(1)
         yield 10 * random()
