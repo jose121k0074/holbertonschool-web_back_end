67eae40109b16dc7ec86f8868abd399ad37f1dad
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-04 15:59:14 +0000

    task 2 - floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" takes a float n as argument and returns the floor of the float
+    Arguments: n: float"""
+
+
+def floor(n: float) -> int:
+    """Return largest int value less than or equal to n."""
+    return int(n) if n >= 0 else int(n) - 1
