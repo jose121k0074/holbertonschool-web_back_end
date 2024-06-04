b2ac2834337e797c82d4a54ec886ee759431a165
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-04 20:49:35 +0000

    fix bug 0

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python3
-""" 
-Write a coroutine called async_generator that takes no arguments
-"""
+"""Write a coroutine called async_generator"""
 
 from asyncio import sleep
 from random import random
@@ -9,8 +7,7 @@ from typing import Generator
 
 
 async def async_generator() -> Generator[float, None, None]:
-    """Generator that yields a random value betwen
-       0 and 10 every second"""
+    """Generator that yields a random value betwen 0 - 10"""
     for i in range(10):
         await sleep(1)
         yield 10 * random()
