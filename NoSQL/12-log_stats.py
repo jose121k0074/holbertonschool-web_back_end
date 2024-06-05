5c07714cea7a8c8e14c171c25e2fd4781bcafc3c
jose daniel puc poot <josedaniel.pucpoot@gmail.com>
2024-06-05 01:16:38 +0000

    fix 12

--- NoSQL/12-log_stats.py
+++ NoSQL/12-log_stats.py
@@ -11,13 +11,13 @@ if __name__ == "__main__":
 
     n_logs = nginx_collection.count_documents({})
     print(f'{n_logs} logs')
+
     methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
     print('Methods:')
     for method in methods:
         count = nginx_collection.count_documents({"method": method})
         print(f'\tmethod {method}: {count}')
 
-    status_check = nginx_collection.count_documents(
-        {"method": "GET", "path": "/status"}
-        )
-    print(f'{status_check} status check')
+    status_check = nginx_collection.count_documents({"method": "GET",
+                                                     "path": "/status"})
+    print(f"{status_check} status check")
